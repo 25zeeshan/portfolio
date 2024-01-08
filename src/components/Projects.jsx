9e0e0176902567fff43068d7101e43e426206388
25zeeshan <25zeeshanraza@gmail.com>
2024-01-08 20:19:32 +0530

    responsive css added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/prop-types */
+
 import "./Projects.css";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const projects = [
   {
@@ -7,8 +10,8 @@ const projects = [
     image: "/projects/chessAnalysis.png",
     description: "Setup and analyze custom chess positions",
     url: "https://chess-analysis.vercel.app/analysis",
-    tech : "React, Typescript, HTML, CSS, Python, django",
-    github: "https://github.com/25zeeshan/Chess-Analysis"
+    tech: "React, Typescript, HTML, CSS, Python, django",
+    github: "https://github.com/25zeeshan/Chess-Analysis",
   },
   {
     name: "Sort Visualizer",
@@ -16,7 +19,7 @@ const projects = [
     description: "Visualization of sorting algorithms",
     url: "https://25zeeshan.github.io/Sort-Visualize/",
     github: "https://github.com/25zeeshan/Sort-Visualize",
-    tech : "Bootstrap, React.js, HTML, CSS",
+    tech: "Bootstrap, React.js, HTML, CSS",
   },
   {
     name: "Wordle clone",
@@ -24,31 +27,30 @@ const projects = [
     description: "Clone of popular word game wordle",
     url: "https://chess-analysis.vercel.app/analysis",
     github: "https://github.com/25zeeshan/Wordle_Clone",
-    tech : "JavaScript, HTML, CSS",
+    tech: "JavaScript, HTML, CSS",
   },
 ];
 
 const ProjectDetail = (props) => {
-  console.log(props);
   return (
     <div className="project-detail">
       <div className="image-container">
         <div className="overlay">
-          <div >
+          <div>
             <a href={props.url} target="_">
               {props.name}
             </a>
-            <a href={props.github} target="_"><img src="/github.svg" style={{width : "30px"}}/></a>
-            <a href={props.url} target="_">< img src="/link.png" style={{width : "25px"}}/></a>
-            
+            <a href={props.github} target="_">
+              <img src="/github.svg" className="project-links"/>
+            </a>
+            <a href={props.url} target="_">
+              <img src="/link.png" className="project-links second" />
+            </a>
           </div>
-          <p>{props.description}</p>
-          <div>
-            {props.tech}
-            
-          </div>
+          <p className="project-description">{props.description}</p>
+          <div className="project-tech">{props.tech}</div>
         </div>
-        <img src={props.image} alt={props.name} />
+        <img src={props.image} alt={props.name} className="aquascale"/>
       </div>
     </div>
   );
@@ -59,12 +61,14 @@ const Projects = function () {
   return (
     <div id="projects" className="projects new-section">
       <div className="heading">
-        Some things I&apos;ve built<div className="headingLine"></div>
+        Some things I&apos;ve built
       </div>
 
-      {projects.map((project, index) => {
-        return <ProjectDetail key={index} {...project} />;
-      })}
+      <Carousel useKeyboardArrows={true}>
+        {projects.map((project, index) => {
+          return <ProjectDetail key={index} {...project} />;
+        })}
+      </Carousel>
     </div>
   );
 };
